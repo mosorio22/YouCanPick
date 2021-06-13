42f6887c7a439c3920c738d6763bd0ca825ac8cd
mosorio22 <moso4410@gmail.com>
2021-06-13 10:17:11 -0400

    js fix for add more ordering

--- static/js/home.js
+++ static/js/home.js
@@ -2,13 +2,14 @@
 
 function addMoreFields() {
 	var inputCount = document.getElementsByTagName('input').length;
+	var addButton = document.getElementById('add-more');
 	if (inputCount <= 10) {
 		var input = document.createElement('input');
 		input.name = "input" + inputCount;
 		input.type = "text";
 		input.required =true;
 		var form = document.getElementsByClassName('home-page-form')[0];
-		form.insertBefore(input, form[0])
+		form.insertBefore(input, addButton);
 	}
 	else {
 		alert("Please limit yourselves to 10 options, we can't make all of the decisions!");
