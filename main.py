6a345c0b10b732f7f489d52739eb540c022bfcd9
7mt <gmsp.mark@gmail.com>
2021-06-29 22:02:04 -0400

    Simplify module import. Resolve PEP format warnings.

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 from flask import Flask, render_template, request
 import random
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-import src
-from you_can_pick import you_can_pick
+from src.you_can_pick import you_can_pick
+
 app = Flask(__name__)
 
 
-#home page
+# home page
 @app.route("/", methods=['GET', 'POST'])
 def home():
-	if request.method == "POST":
-		request_data = request.form
-		random_restaurant = you_can_pick(request_data)
-		return render_template("home.html", random_restaurant =  random_restaurant)
-	return render_template("home.html")
+    if request.method == "POST":
+        request_data = request.form
+        random_restaurant = you_can_pick(request_data)
+        return render_template("home.html", random_restaurant=random_restaurant)
+    return render_template("home.html")
 
-#we can pick page
+
+# we can pick page
 @app.route("/we_can_pick", methods=['GET', 'POST'])
 def we_can_pick():
-	if request.method == "POST":
-		request_data = list(request.form.values())
-		random_restaurant = random.choice(request_data)
-		return render_template("we_can_pick.html", random_restaurant = random_restaurant)
-	return render_template("we_can_pick.html")
+    if request.method == "POST":
+        request_data = list(request.form.values())
+        random_restaurant = random.choice(request_data)
+        return render_template("we_can_pick.html", random_restaurant=random_restaurant)
+    return render_template("we_can_pick.html")
 
-#about page
+
+# about page
 @app.route("/about")
 def about():
-	return render_template("about.html")
+    return render_template("about.html")
+
 
 @app.errorhandler(KeyError)
 def key_error(e):
-	return render_template('home.html', e=e), 500
+    return render_template('home.html', e=e), 500
 
-if __name__ == "__main__":
-	app.run()
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
